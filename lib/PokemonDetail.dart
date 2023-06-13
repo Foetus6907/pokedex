24b10368c2e85a5117f4a364804c0959a172a474
Foetus6907 <mghoubali@gmail.com>
2023-06-13 18:15:14 +0200

    Add french pokemon description

--- lib/PokemonDetail.dart
+++ lib/PokemonDetail.dart
@@ -10,23 +10,132 @@ class PokemonDetail extends StatelessWidget {
   // add in this query the pokemonId class variable
   final String query = """
   query (\$id: Int!) {
-    pokemon_v2_pokemon(where: {id: {_eq: \$id}}) {
-      name
-      id
-      height
-      base_experience
-      weight
-      pokemon_v2_pokemontypes {
-        pokemon_v2_type {
-          name
+      pokemon_v2_pokemon(where: {id: {_eq: \$id}}) {
+        name
+        id
+        height
+        base_experience
+        weight
+        pokemon_v2_pokemontypes {
+          pokemon_v2_type {
+            name
+          }
         }
-      }
       pokemon_v2_pokemonsprites {
         sprites
       }
+      pokemon_v2_pokemonspecy {
+        name
+        is_mythical
+        is_legendary
+        is_baby
+        pokemon_v2_pokemonspeciesflavortexts(where: {language_id: {_eq: 5}}) {
+          flavor_text
+        }
+        pokemon_v2_pokemons {
+          pokemon_v2_pokemonspecy {
+            pokemon_v2_pokemonspeciesnames(where: {language_id: {_eq: 5}}) {
+              name
+            }
+          }
+        }
+      }
     }
   }
-  """;
+""";
+/* Query response id : 
+
+{
+  "data": {
+    "pokemon_v2_pokemon": [
+      {
+        "name": "caterpie",
+        "id": 10,
+        "height": 3,
+        "base_experience": 39,
+        "weight": 29,
+        "pokemon_v2_pokemontypes": [
+          {
+            "pokemon_v2_type": {
+              "name": "bug"
+            }
+          }
+        ],
+        "pokemon_v2_pokemonsprites": [
+          {
+            "sprites": "{\"front_default\": \"/media/sprites/pokemon/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/shiny/10.png\", \"front_shiny_female\": null, \"back_default\": \"/media/sprites/pokemon/back/10.png\", \"back_female\": null, \"back_shiny\": \"/media/sprites/pokemon/back/shiny/10.png\", \"back_shiny_female\": null, \"other\": {\"dream_world\": {\"front_default\": \"/media/sprites/pokemon/other/dream-world/10.svg\", \"front_female\": null}, \"home\": {\"front_default\": \"/media/sprites/pokemon/other/home/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/other/home/shiny/10.png\", \"front_shiny_female\": null}, \"official-artwork\": {\"front_default\": \"/media/sprites/pokemon/other/official-artwork/10.png\", \"front_shiny\": \"/media/sprites/pokemon/other/official-artwork/shiny/10.png\"}}, \"versions\": {\"generation-i\": {\"red-blue\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-i/red-blue/10.png\", \"front_gray\": \"/media/sprites/pokemon/versions/generation-i/red-blue/gray/10.png\", \"back_default\": \"/media/sprites/pokemon/versions/generation-i/red-blue/back/10.png\", \"back_gray\": \"/media/sprites/pokemon/versions/generation-i/red-blue/back/gray/10.png\", \"front_transparent\": \"/media/sprites/pokemon/versions/generation-i/red-blue/transparent/10.png\", \"back_transparent\": \"/media/sprites/pokemon/versions/generation-i/red-blue/transparent/back/10.png\"}, \"yellow\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-i/yellow/10.png\", \"front_gray\": \"/media/sprites/pokemon/versions/generation-i/yellow/gray/10.png\", \"back_default\": \"/media/sprites/pokemon/versions/generation-i/yellow/back/10.png\", \"back_gray\": \"/media/sprites/pokemon/versions/generation-i/yellow/back/gray/10.png\", \"front_transparent\": \"/media/sprites/pokemon/versions/generation-i/yellow/transparent/10.png\", \"back_transparent\": \"/media/sprites/pokemon/versions/generation-i/yellow/transparent/back/10.png\"}}, \"generation-ii\": {\"crystal\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-ii/crystal/10.png\", \"front_shiny\": \"/media/sprites/pokemon/versions/generation-ii/crystal/shiny/10.png\", \"back_default\": \"/media/sprites/pokemon/versions/generation-ii/crystal/back/10.png\", \"back_shiny\": \"/media/sprites/pokemon/versions/generation-ii/crystal/back/shiny/10.png\", \"front_transparent\": \"/media/sprites/pokemon/versions/generation-ii/crystal/transparent/10.png\", \"front_shiny_transparent\": \"/media/sprites/pokemon/versions/generation-ii/crystal/transparent/shiny/10.png\", \"back_transparent\": \"/media/sprites/pokemon/versions/generation-ii/crystal/transparent/back/10.png\", \"back_shiny_transparent\": \"/media/sprites/pokemon/versions/generation-ii/crystal/transparent/back/shiny/10.png\"}, \"gold\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-ii/gold/10.png\", \"front_shiny\": \"/media/sprites/pokemon/versions/generation-ii/gold/shiny/10.png\", \"back_default\": \"/media/sprites/pokemon/versions/generation-ii/gold/back/10.png\", \"back_shiny\": \"/media/sprites/pokemon/versions/generation-ii/gold/back/shiny/10.png\", \"front_transparent\": \"/media/sprites/pokemon/versions/generation-ii/gold/transparent/10.png\"}, \"silver\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-ii/silver/10.png\", \"front_shiny\": \"/media/sprites/pokemon/versions/generation-ii/silver/shiny/10.png\", \"back_default\": \"/media/sprites/pokemon/versions/generation-ii/silver/back/10.png\", \"back_shiny\": \"/media/sprites/pokemon/versions/generation-ii/silver/back/shiny/10.png\", \"front_transparent\": \"/media/sprites/pokemon/versions/generation-ii/silver/transparent/10.png\"}}, \"generation-iii\": {\"emerald\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-iii/emerald/10.png\", \"front_shiny\": \"/media/sprites/pokemon/versions/generation-iii/emerald/shiny/10.png\"}, \"firered-leafgreen\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-iii/firered-leafgreen/10.png\", \"front_shiny\": \"/media/sprites/pokemon/versions/generation-iii/firered-leafgreen/shiny/10.png\", \"back_default\": \"/media/sprites/pokemon/versions/generation-iii/firered-leafgreen/back/10.png\", \"back_shiny\": \"/media/sprites/pokemon/versions/generation-iii/firered-leafgreen/back/shiny/10.png\"}, \"ruby-sapphire\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-iii/ruby-sapphire/10.png\", \"front_shiny\": \"/media/sprites/pokemon/versions/generation-iii/ruby-sapphire/shiny/10.png\", \"back_default\": \"/media/sprites/pokemon/versions/generation-iii/ruby-sapphire/back/10.png\", \"back_shiny\": \"/media/sprites/pokemon/versions/generation-iii/ruby-sapphire/back/shiny/10.png\"}}, \"generation-iv\": {\"diamond-pearl\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-iv/diamond-pearl/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-iv/diamond-pearl/shiny/10.png\", \"front_shiny_female\": null, \"back_default\": \"/media/sprites/pokemon/versions/generation-iv/diamond-pearl/back/10.png\", \"back_female\": null, \"back_shiny\": \"/media/sprites/pokemon/versions/generation-iv/diamond-pearl/back/shiny/10.png\", \"back_shiny_female\": null}, \"heartgold-soulsilver\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/shiny/10.png\", \"front_shiny_female\": null, \"back_default\": \"/media/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/back/10.png\", \"back_female\": null, \"back_shiny\": \"/media/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/back/shiny/10.png\", \"back_shiny_female\": null}, \"platinum\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-iv/platinum/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-iv/platinum/shiny/10.png\", \"front_shiny_female\": null, \"back_default\": \"/media/sprites/pokemon/versions/generation-iv/platinum/back/10.png\", \"back_female\": null, \"back_shiny\": \"/media/sprites/pokemon/versions/generation-iv/platinum/back/shiny/10.png\", \"back_shiny_female\": null}}, \"generation-v\": {\"black-white\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-v/black-white/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-v/black-white/shiny/10.png\", \"front_shiny_female\": null, \"back_default\": \"/media/sprites/pokemon/versions/generation-v/black-white/back/10.png\", \"back_female\": null, \"back_shiny\": \"/media/sprites/pokemon/versions/generation-v/black-white/back/shiny/10.png\", \"back_shiny_female\": null, \"animated\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-v/black-white/animated/10.gif\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-v/black-white/animated/shiny/10.gif\", \"front_shiny_female\": null, \"back_default\": \"/media/sprites/pokemon/versions/generation-v/black-white/animated/back/10.gif\", \"back_female\": null, \"back_shiny\": \"/media/sprites/pokemon/versions/generation-v/black-white/animated/back/shiny/10.gif\", \"back_shiny_female\": null}}}, \"generation-vi\": {\"omegaruby-alphasapphire\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire/shiny/10.png\", \"front_shiny_female\": null}, \"x-y\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-vi/x-y/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-vi/x-y/shiny/10.png\", \"front_shiny_female\": null}}, \"generation-vii\": {\"ultra-sun-ultra-moon\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/10.png\", \"front_female\": null, \"front_shiny\": \"/media/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/shiny/10.png\", \"front_shiny_female\": null}, \"icons\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-vii/icons/10.png\", \"front_female\": null}}, \"generation-viii\": {\"icons\": {\"front_default\": \"/media/sprites/pokemon/versions/generation-viii/icons/10.png\", \"front_female\": null}}}}"
+          }
+        ],
+        "pokemon_v2_pokemonspecy": {
+          "name": "caterpie",
+          "is_mythical": false,
+          "is_legendary": false,
+          "is_baby": false,
+          "pokemon_v2_pokemonspeciesflavortexts": [
+            {
+              "flavor_text": "Ses antennes rouges libèrent une\npuanteur qui repousse l’ennemi.\nIl grandit par mues régulières."
+            },
+            {
+              "flavor_text": "Ses antennes rouges libèrent une\npuanteur qui repousse l’ennemi.\nIl grandit par mues régulières."
+            },
+            {
+              "flavor_text": "Pour se protéger, il émet un gaz puant par ses\nantennes, qui fait fuir ses ennemis audacieux."
+            },
+            {
+              "flavor_text": "Ses pattes ont des ventouses lui permettant de\ngrimper sur toute surface, notamment les arbres."
+            },
+            {
+              "flavor_text": "Chenipan a un appétit d’ogre. Il peut engloutir des feuilles\nplus grosses que lui. Les antennes de ce Pokémon dégagent\nune odeur particulièrement entêtante."
+            },
+            {
+              "flavor_text": "Chenipan a un appétit d’ogre. Il peut engloutir des feuilles\nplus grosses que lui. Les antennes de ce Pokémon dégagent\nune odeur particulièrement entêtante."
+            },
+            {
+              "flavor_text": "Lorsqu’il est attaqué par un Pokémon Vol,\nil utilise ses antennes pour dégager une odeur\nnauséabonde, mais cela le sauve rarement."
+            },
+            {
+              "flavor_text": "Un Pokémon facile à attraper et qui croît\nrapidement. C’est l’un des partenaires\nprivilégiés des Dresseurs débutants."
+            },
+            {
+              "flavor_text": "C’est peut-être parce qu’il a envie de grandir\nle plus vite possible qu’il est si vorace.\nIl engloutit une centaine de feuilles par jour."
+            },
+            {
+              "flavor_text": "Son corps est mou et sans force.\nLa nature semble l’avoir destiné à servir\nde proie aux autres Pokémon."
+            },
+            {
+              "flavor_text": "Quand on touche l’appendice sur son front,\nil sécrète une odeur nauséabonde pour se\nprotéger."
+            },
+            {
+              "flavor_text": "Quand on touche l’appendice sur son front,\nil sécrète une odeur nauséabonde pour se\nprotéger."
+            },
+            {
+              "flavor_text": "Pour se protéger, il émet par ses antennes\nune odeur nauséabonde qui fait fuir ses ennemis."
+            },
+            {
+              "flavor_text": "Ses petites pattes munies de ventouses\nlui permettent de monter les pentes\net de grimper aux murs sans se fatiguer."
+            }
+          ],
+          "pokemon_v2_pokemons": [
+            {
+              "pokemon_v2_pokemonspecy": {
+                "pokemon_v2_pokemonspeciesnames": [
+                  {
+                    "name": "Chenipan"
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
+
+
+
+
+*/
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +171,8 @@ class PokemonDetail extends StatelessWidget {
               }
 
               final pokemon = result.data?['pokemon_v2_pokemon'][0];
+              final pokemonDescription = pokemon['pokemon_v2_pokemonspecy']
+                  ['pokemon_v2_pokemonspeciesflavortexts'][0]['flavor_text'];
 
               // print in the console the pokemon data
               final String sprites =
@@ -117,7 +228,13 @@ class PokemonDetail extends StatelessWidget {
                         });
                       }),
                     ),
-                    /*Make this prettier display date in an elegant table */
+                    /*Display pokemon flavor in a simple Card*/
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(pokemonDescription),
+                      ),
+                    ),
                     ListTile(
                       leading: CircleAvatar(
                           backgroundColor: Colors.transparent,
